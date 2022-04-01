107236116fc48a1102cec4fca0177067a64d2612
Marnikilis <diana@cravter.com>
2022-04-01 22:20:50 +0300

    add interface

--- src/components/Converter/Converter.tsx
+++ src/components/Converter/Converter.tsx
@@ -1,16 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState, FC } from 'react';
 // @ts-ignore
 import styles from './Converter.module.scss';
-import {Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
+interface Props {
+  val: number;
+  cur: string;
+}
 
-const Converter = ({val, cur}:any)=> {
+const Converter: FC<Props> = ({val, cur}: Props) => {
 
   const [amountIn, setAmountIn] = useState<number>(1);
-  const [amountTo, setAmountTo] = useState<string | null>(' ');
+  const [amountTo, setAmountTo] = useState('');
 
   useEffect(() => {
-    setAmountTo((1/val).toFixed(3))
+    setAmountTo((1 / val).toFixed(3))
     setAmountIn(1)
   }, [val])
 
@@ -22,13 +26,14 @@ const Converter = ({val, cur}:any)=> {
 
   return (
     <div className={styles.container}>
-      <input type="number" value={amountIn} onChange={(e) => setAmountIn(e.target.valueAsNumber)}/>
+      <input type="number"
+             value={amountIn}
+             onChange={(e) => setAmountIn(e.target.valueAsNumber)}/>
       <label>UAH</label>
       &rArr;
-      <input
-             value={amountTo}
-             onChange={() => setAmountTo(val)}
-             placeholder={(1/val).toFixed(3)}
+      <input value={amountTo}
+             onChange={() => setAmountTo(`${val}`)}
+             placeholder={(1 / val).toFixed(3)}
       />
       <label>{cur}</label>
       <Button variant="outline-dark" onClick={onExChange}>Exchange</Button>
