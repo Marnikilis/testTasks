b14e889c6c2714a8aabcc9847df2811a2e4b1af6
Marnikilis <diana@cravter.com>
2022-04-01 20:57:49 +0300

    change to tsx

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+//@ts-ignore
 import styles from './Loader.module.scss';
 
 const Loader = () => {
   return (
     <div className={styles.ripple}>
-      <div></div>
-      <div></div>
+      <div>
+      </div>
+      <div>
+      </div>
     </div>
   );
 };
